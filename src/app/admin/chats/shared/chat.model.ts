11fa21d4a4c07a688d9089e4eac4b53051d92ab6
Gonzales <grober_gonzales@hotmail.com>
2021-11-24 12:31:39 -0500

    Chat update -> User List

--- src/app/admin/chats/shared/chat.model.ts
+++ src/app/admin/chats/shared/chat.model.ts
@@ -4,6 +4,7 @@ export class Chat {
 	detalles: string;
 	cant_mensajes: number;
 	mensajes: Message[];
+	usuarios: Usuario[];
 }
 
 export class Message {
@@ -12,4 +13,15 @@ export class Message {
 	hora_mensaje: Date;
 	mensaje: string;
 	usuario_id: number;
+	usuario_name: string;
+}
+
+export class Usuario{
+	apellidos: string;
+	correo: string;
+	id: number;
+	isPremium: boolean;
+	nombres: string;
+	num_monedas: number;
+	tipo_usuario: string;
 }
